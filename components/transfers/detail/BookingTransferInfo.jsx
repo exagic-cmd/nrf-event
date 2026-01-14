e8c89c7913ee7b7588092c482a65e5030dcb6dee
Iffat Ayesha <iffatayesha4@gmail.com>
2026-01-14 18:58:07 +0500

    update

--- components/transfers/detail/BookingTransferInfo.jsx
+++ components/transfers/detail/BookingTransferInfo.jsx
@@ -77,7 +77,7 @@ const DatePickerField = ({
           ? "bg-orange-100 text-gray-700 hover:bg-orange-200"
           : ""
       }
-      className={`text-base w-full px-4 md:py-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-orange-200 ${
+      className={`text-base w-full px-4 md:py-3 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-gray-200 ${
         disabled ? "bg-gray-100 cursor-not-allowed" : ""
       }`}
       wrapperClassName="w-full"
