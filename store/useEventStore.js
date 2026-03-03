8b89efe18ad36d42e457642c5020971bf5ca8f57
Iffat Ayesha <iffatayesha4@gmail.com>
2026-03-03 16:12:04 +0500

    GA trackign id  added and event on not published navigated to error iffat

--- store/useEventStore.js
+++ store/useEventStore.js
@@ -5,14 +5,14 @@ import { persist, createJSONStorage } from "zustand/middleware";
 export const useEventStore = create(
   persist(
     (set, get) => ({
-      event: [],
+      event: null,
       lastFetched: null,       // add timestamp
       isLoading: false,
       error: null,
 
       setEvent: data => set({ event: data, lastFetched: Date.now() }),
 
-      FetchEvent: async () => {
+      FetchEvent: async (router) => {
         set({ isLoading: true, error: null });
 
         try {
@@ -24,22 +24,34 @@ export const useEventStore = create(
           if (!res.ok) throw new Error("Network response was not ok");
 
           const json = await res.json();
-          const eventData = json?.data || [];
+
+          // If not published redirect to the error page.
+          if (json?.data?.event?.is_published === 0) {
+            const message = encodeURIComponent("This event is not currently available.");
+            if (router) {
+              router.push(`/error?message=${message}`);
+            } else {
+              console.warn("Router instance not passed to FetchEvent. Using window.location for redirection.");
+              window.location.href = `/error?message=${message}`;
+            }
+            set({ isLoading: false, error: "Event not published." });
+            return null;
+          }
+
+          const eventData = json?.data || null;
 
           set({
             event: eventData,
             lastFetched: Date.now(),
             isLoading: false
           });
-
           return eventData;
         } catch (err) {
           set({
             isLoading: false,
             error: err.message || "Failed to fetch event"
           });
-
-          return [];
+          return null;
         }
       }
     }),
